ba3fe6b37bed6ba895504d572e748759a26a8fc6
DorotaOrzeszek <dorota.orzeszek@gmail.com>
2016-05-27 14:26:17 +0200

    Addes default Settings screen

--- app/src/main/java/com/dorotaorzeszek/todomanager/AddToDoActivity.java
+++ app/src/main/java/com/dorotaorzeszek/todomanager/AddToDoActivity.java
@@ -1,5 +1,6 @@
 package com.dorotaorzeszek.todomanager;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,4 +11,15 @@ public class AddToDoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_to_do);
     }
+    /**
+     * Set up the {@link android.app.ActionBar}, if the API is available.
+     */
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            // Show the Up button in the action bar.
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
 }
