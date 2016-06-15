396bdd7d531b180b16aae1ab204c3d4fcb0bc03c
DorotaOrzeszek <dorota.orzeszek@gmail.com>
2016-06-15 22:07:16 +0200

    Code cleanup 3

--- app/src/main/java/com/dorotaorzeszek/todomanager/AddToDoActivity.java
+++ app/src/main/java/com/dorotaorzeszek/todomanager/AddToDoActivity.java
@@ -1,8 +1,13 @@
 package com.dorotaorzeszek.todomanager;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddToDoActivity extends AppCompatActivity {
 
@@ -10,7 +15,20 @@ public class AddToDoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_to_do);
+
+/*        R.id.addToDoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = getApplicationContext();
+                Intent intent = new Intent(context, ListActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intent);
+            }
+        });
+    */
     }
+
+
     /**
      * Set up the {@link android.app.ActionBar}, if the API is available.
      */
